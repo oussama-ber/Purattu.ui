6a18168990e2b68ab62d65131a3e5b369161d991
salimmansi <salim.mansi@outlook.com>
2024-02-07 17:53:24 +0100

    iiiiiiii

--- src/app/components/landingpage/landingpage.component.ts
+++ src/app/components/landingpage/landingpage.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-landingpage',
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
-export class LandingpageComponent {
+export class LandingpageComponent implements OnInit {
+  videoURL: string = "1ozGKlOzEVc"; 
+  safeURL: SafeResourceUrl | undefined;
 
+  constructor(private _sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.videoURL}`);
+  }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 let origin = window.location.origin.toLowerCase();
 let getApiBaseUrl = "";
 if(origin.includes("localhost")){
-  getApiBaseUrl = "http://localhost:3000";
+  getApiBaseUrl = "https://purattu-api.onrender.com";
 }else{
   getApiBaseUrl = "https://purattu-api.onrender.com";
 }
