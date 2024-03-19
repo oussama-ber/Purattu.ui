c712b184253dcc98d5f7b6f376bb5a5653d6df7d
oussema <oussema.benrejab@gmail.com>
2024-03-20 00:08:04 +0100

    delete comments

--- src/app/components/blog-detail/blog-detail.component.ts
+++ src/app/components/blog-detail/blog-detail.component.ts
@@ -15,7 +15,6 @@ export class BlogDetailComponent implements OnInit {
   currentBlogId: string = '';
   currentBlog: Blog = new Blog();
   fetchedBlogIsfetched: boolean = false;
-  imageUrl: string = 'https://firebasestorage.googleapis.com/v0/b/purattu.appspot.com/o/BlogsImages%2FScreenshot%202023-04-02%20024224.pngMon%20Feb%2019%202024%2016%3A36%3A10%20GMT%2B0100%20(GMT%2B01%3A00)?alt=media&token=e7c1c9f4-7130-48aa-99ba-59e2b85f8c5e';
 
   //#endregion Variables
   constructor(private route: ActivatedRoute) {
